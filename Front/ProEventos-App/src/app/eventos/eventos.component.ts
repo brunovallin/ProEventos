d9fd5cc1e30fb2a2abc1c164f3524d9461341e38
Bruno Vallin <brunovallin@hotmail.com>
2023-08-16 05:18:51 -0300

    Seção 5: Angular, Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
+  public eventos: any = [];
+  public eventosFiltrados:any = [];
+  widthImg:number = 70;
+  marginImg:number = 2;
+  imgCollapse = true;
+  private _listFilter:string = '';
 
-  public eventos: any;
+  public get listFilter(){
+    return this._listFilter;
+  }
+
+  public set listFilter(value:string){
+    this._listFilter = value;
+    this.eventosFiltrados = this.listFilter ? this.filtrarEventos(this.listFilter):this.eventos;
+  }
 
   constructor(private http:HttpClient) {}
 
@@ -17,10 +30,21 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos():void{
-
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
+    );
+
+  }
+
+  filtrarEventos(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
     );
   }
 }
